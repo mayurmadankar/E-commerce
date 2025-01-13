78b04d168c8d2112dc8fcc7ea223d843daead7b4
Mayur <madankarmayur5@gmail.com>
2025-01-13 16:26:08 +0530

    initial commit

---
+++ client/server.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import { connectUsingMongoose } from "./config/db.js";
+import cookieParser from "cookieParser";
+import cors from "cors";
+
+const server = express();
+const port = process.env.PORT;
+
+server.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expries",
+      "pragma"
+    ],
+    credentials: true
+  })
+);
+server.use(cookieParser());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("Welcome to the ecommerce application");
+});
+
+server.listen(port, () => {
+  console.log(`server is listening at port number ${port}`);
+  connectUsingMongoose();
+});
---
+++ client/src/store/auth-slice/authSlice.js
@@ -0,0 +1,1 @@
+const authSlice=createSlice
